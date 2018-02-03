ff226d8a4d3a3603ef1641a160ba4ea7d98a2ee2
davmarra <d.marrancone@campus.uniurb.it>
2018-02-03 01:33:10 +0100

    Add files via upload

---
+++ prova_location.php
@@ -0,0 +1,42 @@
+<?php
+$username="root";
+$password="";
+$database="userlocation";
+
+// Gets data from URL parameters.
+$name = $_GET['name'];
+$address = $_GET['address'];
+$type = $_GET['type'];
+$lat = $_GET['lat'];
+$lng = $_GET['lng'];
+$ascensore = $_GET['ascensore'];
+$rampa= $_GET['rampa'];
+$serviziigienici = $_GET['serviziigienici'];
+
+// Opens a connection to a MySQL server.
+$connection=mysqli_connect ("localhost", $username, $password, $database);
+if (!$connection) {
+  die('Not connected : ' . mysql_error());
+}
+
+// Inserts new row with place data.
+$query = sprintf("INSERT INTO locationsend " .
+         " (id, name, address, type, lat, lng, ascensore, rampa, serviziigienici ) " .
+         " VALUES (NULL, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');",
+         mysqli_real_escape_string($connection, $name),
+         mysqli_real_escape_string($connection, $address),
+		 mysqli_real_escape_string($connection, $type),
+         mysqli_real_escape_string($connection, $lat),
+         mysqli_real_escape_string($connection, $lng),
+		 mysqli_real_escape_string($connection, $ascensore),
+		 mysqli_real_escape_string($connection, $rampa),
+		 mysqli_real_escape_string($connection, $serviziigienici));
+         
+
+$result = mysqli_query($connection, $query);
+
+if (!$result) {
+  die('Invalid query: ' . mysql_error());
+}
+
+?>
